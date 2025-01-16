52dd461bed467a3fa8a154ff7b03de40e64068ab
LouisLP <louis.lascelles.palys@gmail.com>
2025-01-16 21:06:05 +0100

    Using the new spinner while loading/fetching data

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SingleApplication from "./SingleApplication";
 import { Button } from "./ui/Button/Button";
+import { Spinner } from "./ui/Spinner/Spinner";
 import styles from "./Applications.module.css";
 import { Application } from "./types/Application";
 
@@ -8,22 +9,26 @@ const Applications = () => {
   const [applications, setApplications] = useState<Application[]>([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
   const LIMIT = 5;
 
   const fetchApplications = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch(
         `http://localhost:3001/api/applications?_page=${page}&_limit=${LIMIT}`
       );
       const data = await response.json();
 
-      // Checking for more pages
       const linkHeader = response.headers.get("Link");
       setHasMore(linkHeader?.includes('rel="next"') ?? false);
 
       setApplications((prev) => [...prev, ...data]);
     } catch (error) {
       console.error("Failed to fetch applications:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -36,8 +41,14 @@ const Applications = () => {
       {applications.map((application) => (
         <SingleApplication key={application.id} application={application} />
       ))}
-      {hasMore && (
-        <Button onClick={() => setPage((p) => p + 1)}>Load more</Button>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        hasMore && (
+          <Button onClick={() => setPage((p) => p + 1)} disabled={isLoading}>
+            Load more
+          </Button>
+        )
       )}
     </div>
   );
