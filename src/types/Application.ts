5fcc28b6c0b6f15229dca805bb90cfd66570c611
LouisLP <louis.lascelles.palys@gmail.com>
2025-01-16 20:19:48 +0100

    Typing for each Application

---
+++ src/types/Application.ts
@@ -0,0 +1,10 @@
+export type Application = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  company: string;
+  email: string;
+  loan_amount: number;
+  date_created: string;
+  expiry_date: string;
+};
